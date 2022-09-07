59d38a5d35504f7b261d2e90e481fd6876bad76b
hridoye <hridoysagor171@gmail.comm>
2022-09-07 16:25:18 -0600

    js event complete

--- ecommerce_header.js
+++ ecommerce_header.js
@@ -15,6 +15,40 @@
     }
     hamburgerMenu();
 
+    function searchPopUp() {
+        const s0401_header_search_id = document.getElementById("s0401_header_search_id");
+        s0401_header_search_id.addEventListener("click", function () {
+            document.getElementById("s0401_search_box_area").classList.add("s0401_search_popup");
+        });
+        const s0401_search_popup_close_btn = document.getElementById("s0401_search_popup_close_btn");
+        s0401_search_popup_close_btn.addEventListener("click", function () {
+            document.getElementById("s0401_search_box_area").classList.remove("s0401_search_popup");
+        });
+    };
+    searchPopUp();
+
+
+    function cartWishlistEvent() {
+
+        const headerMenuItems = document.querySelectorAll(".s0401_header_icon_item_common");
+        for (let i = 0; i < headerMenuItems.length; i++) {
+            const headerMenuItem = headerMenuItems[i];
+            headerMenuItem.addEventListener("click", function () {
+                removeActiveFill();
+                headerMenuItem.classList.add("active_fill_icon");
+            });
+        }
+
+    }
+    cartWishlistEvent();
+
+    function removeActiveFill() {
+        const headerMenuItems = document.querySelectorAll(".s0401_header_icon_item_common");
+        for (let i = 0; i < headerMenuItems.length; i++) {
+            const headerMenuItem = headerMenuItems[i];
+            headerMenuItem.classList.contains("active_fill_icon") && headerMenuItem.classList.remove("active_fill_icon")
+        };
+    };
 
 
 })();
